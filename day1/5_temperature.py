80bd4be58c31e016c7660ab2ad9f284cda20db9d
fancilivain <meigangwen@gmail.com>
2025-07-13 11:57:52 +0800

    Update 5_temperature.py

--- day1/5_temperature.py
+++ day1/5_temperature.py
@@ -5,6 +5,7 @@ from google.genai import types
 GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
 client = genai.Client(api_key=GOOGLE_API_KEY)
 
+
 high_temp_config = types.GenerateContentConfig(temperature=2.0)
 print("High temp results:")
 for _ in range(5):
@@ -26,3 +27,22 @@ for _ in range(5):
     )
     if response.text:
         print(response.text, '-' * 25)
+
+print("-" * 50)
+print("-" * 50)
+
+print("Low temperature story")
+response = client.models.generate_content(
+        model='gemini-2.0-flash',
+        config=types.GenerateContentConfig(temperature=0.0, max_output_tokens=100),
+        contents='Tell me a story about a dragon and a village.'
+)
+print(response.text)
+
+print("High temperature story")
+response = client.models.generate_content(
+        model='gemini-2.0-flash',
+        config=types.GenerateContentConfig(temperature=2.0, max_output_tokens=100),
+        contents='Tell me a story about a dragon and a village.'
+)
+print(response.text)
