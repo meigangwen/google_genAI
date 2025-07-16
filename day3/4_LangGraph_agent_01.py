0e9dcf7baf9987074d74ac897ad531a48cfee3ed
fancilivain <meigangwen@gmail.com>
2025-07-16 15:33:49 +0800

    finished day 3 coding

---
+++ day3/4_LangGraph_agent_01.py
@@ -0,0 +1,87 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+from langchain_google_genai import ChatGoogleGenerativeAI
+from pprint import pprint
+
+class OrderState(TypedDict):
+    """State representing the customer's order conversation."""
+
+    # The chat conversation. This preserves the conversation history
+    # between nodes. The `add_messages` annotation indicates to LangGraph
+    # that state is updated by appending returned messages, not replacing
+    # them.
+    messages: Annotated[list, add_messages]
+
+    # The customer's in-progress order.
+    order: list[str]
+
+    # Flag indicating that the order is placed and completed.
+    finished: bool
+
+
+# The system instruction defines how the chatbot is expected to behave and includes
+# rules for when to call different functions, as well as rules for the conversation, such
+# as tone and what is permitted for discussion.
+BARISTABOT_SYSINT = (
+    "system",  # 'system' indicates the message is a system instruction.
+    "You are a BaristaBot, an interactive cafe ordering system. A human will talk to you about the "
+    "available products you have and you will answer any questions about menu items (and only about "
+    "menu items - no off-topic discussion, but you can chat about the products and their history). "
+    "The customer will place an order for 1 or more items from the menu, which you will structure "
+    "and send to the ordering system after confirming the order with the human. "
+    "\n\n"
+    "Add items to the customer's order with add_to_order, and reset the order with clear_order. "
+    "To see the contents of the order so far, call get_order (this is shown to you, not the user) "
+    "Always confirm_order with the user (double-check) before calling place_order. Calling confirm_order will "
+    "display the order items to the user and returns their response to seeing the list. Their response may contain modifications. "
+    "Always verify and respond with drink and modifier names from the MENU before adding them to the order. "
+    "If you are unsure a drink or modifier matches those on the MENU, ask a question to clarify or redirect. "
+    "You only have the modifiers listed on the menu. "
+    "Once the customer has finished ordering items, Call confirm_order to ensure it is correct then make "
+    "any necessary updates and then call place_order. Once place_order has returned, thank the user and "
+    "say goodbye!"
+    "\n\n"
+    "If any of the tools are unavailable, you can break the fourth wall and tell the user that "
+    "they have not implemented them yet and should keep reading to do so.",
+)
+
+# This is the message with which the system opens the conversation.
+WELCOME_MSG = "Welcome to the BaristaBot cafe. Type `q` to quit. How may I serve you today?"
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+def chatbot(state: OrderState) -> OrderState:
+    """The chatbot itself. A simple wrapper around the model's own chat interface."""
+    message_history = [BARISTABOT_SYSINT] + state["messages"]
+    return {"messages": [llm.invoke(message_history)]}
+    
+
+
+# Set up the initial graph based on our state definition.
+graph_builder = StateGraph(OrderState)
+
+# Add the chatbot function to the app graph as a node called "chatbot".
+graph_builder.add_node("chatbot", chatbot)
+
+# Define the chatbot node as the app entrypoint.
+graph_builder.add_edge(START, "chatbot")
+
+chat_graph = graph_builder.compile()
+print(chat_graph.get_graph().draw_ascii())
+
+# Finished building the chatbot
+
+# Test user interactions
+user_msg = "Hello, what can you do?"
+state = chat_graph.invoke({"messages": [user_msg]})
+
+user_msg = "Oh great, what kinds of latte can you make?"
+
+state["messages"].append(user_msg)
+state = chat_graph.invoke(state)
+
+# pprint(state)
+for msg in state["messages"]:
+    print(f"{type(msg).__name__}: {msg.content}")
---
+++ day3/4_LangGraph_agent_03.py
@@ -0,0 +1,207 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+from langchain_google_genai import ChatGoogleGenerativeAI
+from pprint import pprint
+from langchain_core.messages.ai import AIMessage
+from typing import Literal
+from langchain_core.tools import tool
+from langgraph.prebuilt import ToolNode
+
+class OrderState(TypedDict):
+    """State representing the customer's order conversation."""
+
+    # The chat conversation. This preserves the conversation history
+    # between nodes. The `add_messages` annotation indicates to LangGraph
+    # that state is updated by appending returned messages, not replacing
+    # them.
+    messages: Annotated[list, add_messages]
+
+    # The customer's in-progress order.
+    order: list[str]
+
+    # Flag indicating that the order is placed and completed.
+    finished: bool
+
+
+# The system instruction defines how the chatbot is expected to behave and includes
+# rules for when to call different functions, as well as rules for the conversation, such
+# as tone and what is permitted for discussion.
+BARISTABOT_SYSINT = (
+    "system",  # 'system' indicates the message is a system instruction.
+    "You are a BaristaBot, an interactive cafe ordering system. A human will talk to you about the "
+    "available products you have and you will answer any questions about menu items (and only about "
+    "menu items - no off-topic discussion, but you can chat about the products and their history). "
+    "The customer will place an order for 1 or more items from the menu, which you will structure "
+    "and send to the ordering system after confirming the order with the human. "
+    "\n\n"
+    "Add items to the customer's order with add_to_order, and reset the order with clear_order. "
+    "To see the contents of the order so far, call get_order (this is shown to you, not the user) "
+    "Always confirm_order with the user (double-check) before calling place_order. Calling confirm_order will "
+    "display the order items to the user and returns their response to seeing the list. Their response may contain modifications. "
+    "Always verify and respond with drink and modifier names from the MENU before adding them to the order. "
+    "If you are unsure a drink or modifier matches those on the MENU, ask a question to clarify or redirect. "
+    "You only have the modifiers listed on the menu. "
+    "Once the customer has finished ordering items, Call confirm_order to ensure it is correct then make "
+    "any necessary updates and then call place_order. Once place_order has returned, thank the user and "
+    "say goodbye!"
+    "\n\n"
+    "If any of the tools are unavailable, you can break the fourth wall and tell the user that "
+    "they have not implemented them yet and should keep reading to do so.",
+)
+
+# This is the message with which the system opens the conversation.
+WELCOME_MSG = "Welcome to the BaristaBot cafe. Type `q` to quit. How may I serve you today?"
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+def chatbot(state: OrderState) -> OrderState:
+    """The chatbot itself. A simple wrapper around the model's own chat interface."""
+    message_history = [BARISTABOT_SYSINT] + state["messages"]
+    return {"messages": [llm.invoke(message_history)]}
+
+def human_node(state: OrderState) -> OrderState:
+    """Display the last model message to the user, and receive the user's input."""
+    last_msg = state["messages"][-1]
+    print("Model:", last_msg.content)
+
+    user_input = input("User: ")
+
+    # If it looks like the user is trying to quit, flag the conversation
+    # as over.
+    if user_input in {"q", "quit", "exit", "goodbye"}:
+        state["finished"] = True
+
+    return state | {"messages": [("user", user_input)]}
+
+
+def chatbot_with_welcome_msg(state: OrderState) -> OrderState:
+    """The chatbot itself. A wrapper around the model's own chat interface."""
+
+    if state["messages"]:
+        # If there are messages, continue the conversation with the Gemini model.
+        new_output = llm.invoke([BARISTABOT_SYSINT] + state["messages"])
+    else:
+        # If there are no messages, start with the welcome message.
+        new_output = AIMessage(content=WELCOME_MSG)
+
+    return state | {"messages": [new_output]}
+
+def maybe_exit_human_node(state: OrderState) -> Literal["chatbot", "__end__"]:
+    """Route to the chatbot, unless it looks like the user is exiting."""
+    if state.get("finished", False):
+        return END
+    else:
+        return "chatbot"
+    
+@tool
+def get_menu() -> str:
+    """Provide the latest up-to-date menu."""
+    # Note that this is just hard-coded text, but you could connect this to a live stock
+    # database, or you could use Gemini's multi-modal capabilities and take live photos of
+    # your cafe's chalk menu or the products on the counter and assmble them into an input.
+
+    return """
+    MENU:
+    Coffee Drinks:
+    Espresso
+    Americano
+    Cold Brew
+
+    Coffee Drinks with Milk:
+    Latte
+    Cappuccino
+    Cortado
+    Macchiato
+    Mocha
+    Flat White
+
+    Tea Drinks:
+    English Breakfast Tea
+    Green Tea
+    Earl Grey
+
+    Tea Drinks with Milk:
+    Chai Latte
+    Matcha Latte
+    London Fog
+
+    Other Drinks:
+    Steamer
+    Hot Chocolate
+
+    Modifiers:
+    Milk options: Whole, 2%, Oat, Almond, 2% Lactose Free; Default option: whole
+    Espresso shots: Single, Double, Triple, Quadruple; default: Double
+    Caffeine: Decaf, Regular; default: Regular
+    Hot-Iced: Hot, Iced; Default: Hot
+    Sweeteners (option to add one or more): vanilla sweetener, hazelnut sweetener, caramel sauce, chocolate sauce, sugar free vanilla sweetener
+    Special requests: any reasonable modification that does not involve items not on the menu, for example: 'extra hot', 'one pump', 'half caff', 'extra foam', etc.
+
+    "dirty" means add a shot of espresso to a drink that doesn't usually have it, like "Dirty Chai Latte".
+    "Regular milk" is the same as 'whole milk'.
+    "Sweetened" means add some regular sugar, not a sweetener.
+
+    Soy milk has run out of stock today, so soy is not available.
+  """
+
+# Define the tools and create a "tools" node.
+tools = [get_menu]
+tool_node = ToolNode(tools)
+
+# Attach the tools to the model so that it knows what it can call.
+llm_with_tools = llm.bind_tools(tools)
+
+def maybe_route_to_tools(state: OrderState) -> Literal["tools", "human"]:
+    """Route between human or tool nodes, depending if a tool call is made."""
+    if not (msgs := state.get("messages", [])):
+        raise ValueError(f"No messages found when parsing state: {state}")
+
+    # Only route based on the last message.
+    msg = msgs[-1]
+
+    # When the chatbot returns tool_calls, route to the "tools" node.
+    if hasattr(msg, "tool_calls") and len(msg.tool_calls) > 0:
+        return "tools"
+    else:
+        return "human"
+
+
+def chatbot_with_tools(state: OrderState) -> OrderState:
+    """The chatbot with tools. A simple wrapper around the model's own chat interface."""
+    defaults = {"order": [], "finished": False}
+
+    if state["messages"]:
+        new_output = llm_with_tools.invoke([BARISTABOT_SYSINT] + state["messages"])
+    else:
+        new_output = AIMessage(content=WELCOME_MSG)
+
+    # Set up some defaults if not already set, then pass through the provided state,
+    # overriding only the "messages" field.
+    return defaults | state | {"messages": [new_output]}
+    
+
+graph_builder = StateGraph(OrderState)
+
+# Add the nodes, including the new tool_node.
+graph_builder.add_node("chatbot", chatbot_with_tools)
+graph_builder.add_node("human", human_node)
+graph_builder.add_node("tools", tool_node)
+
+# Chatbot may go to tools, or human.
+graph_builder.add_conditional_edges("chatbot", maybe_route_to_tools)
+# Human may go back to chatbot, or exit.
+graph_builder.add_conditional_edges("human", maybe_exit_human_node)
+
+# Tools always route back to chat afterwards.
+graph_builder.add_edge("tools", "chatbot")
+
+graph_builder.add_edge(START, "chatbot")
+graph_with_menu = graph_builder.compile()
+
+print(graph_with_menu.get_graph().draw_ascii())
+
+# Finished building the chatbot
+config = {"recursion_limit": 100}
+state = graph_with_menu.invoke({"messages": []}, config)
